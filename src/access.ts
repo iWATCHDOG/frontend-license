4533490fa8c111f35203fb3c4b22ac363de21807
花花 <73741466+xLikeWATCHDOG@users.noreply.github.com>
2024-06-16 23:36:14 +0800

    feat(access.ts): Add default value for initialState to prevent undefined errors

--- src/access.ts
+++ src/access.ts
@@ -1,4 +1,11 @@
 export default (initialState: InitialState) => {
+  if (!initialState) {
+    // 处理 initialState 未定义的情况
+    return {
+      canUser: false,
+      canAdmin: false,
+    };
+  }
   const canUser = !!initialState.loginUser;
   // 有登录用户，且有权限*或group.admin
   const canAdmin = (canUser && (initialState.loginUser?.group?.permission === 'group.admin' || initialState.loginUser?.group?.permission === '*'));
